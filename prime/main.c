bcf1dd8fd77bccf0c60ce4811aa7c1cfff620592
samargooda <samarmahmoud368@yahoo.com>
2019-11-29 11:32:15 +0200

    added prime

---
+++ prime/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    int i;
+    int count=0;
+    printf("enter the number");
+    scanf("%d",&num);
+    if(num==0||num==1)
+        printf("this isnot a prime number");
+    else if(num==2||num==3)
+         printf("this is a prime number");
+    else
+       {
+           for(i=2;i<num;i++)
+        {
+            if(num%i==0)
+                count=count+1;
+        }
+           if (count>0)
+
+              printf("this isnot prime");
+
+          else
+              printf("this is a prime");
+       }
+    return 0;
+}
